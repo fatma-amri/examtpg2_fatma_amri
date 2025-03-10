a379131da52652f1fc3ea92ef02c6db22c12995e
fatma <fatmaa.amri22@gmail.com>
2025-03-10 13:40:19 +0100

    Ajout du fichier dossier.py

--- dossier.py
+++ dossier.py
@@ -1,6 +1,13 @@
+
 data=[1,3,5]
 print ("la somme est : ", sum (data))
 print ("le min  est : ", min (data))
 print ("le max est : ", max (data))
 else : 
 	print("dossier vide")
+def somme (T)
+  s=0 
+  for t in T:
+	s+=t
+	return s
+
